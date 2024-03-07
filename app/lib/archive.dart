0b8e638c96dd6cc901e6aafe0a934dfcbf7624ff
Moisés Hidalgo Guerra <mohidalguerra@gmail.com>
2024-03-06 21:29:21 -0500

    Can Build

--- app/lib/archive.dart
+++ app/lib/archive.dart
@@ -10,7 +10,7 @@ import 'package:flutter_i18n/flutter_i18n.dart';
 GlobalKey<ScaffoldState> archiveScaffold = GlobalKey<ScaffoldState>();
 
 class Archive extends StatefulWidget {
-  Archive(Key key) : super(key: key);
+  Archive();
 
   @override
   _ArchiveState createState() => _ArchiveState();
