128cbb72075adab3ab9583b6446d62cdb9554af6
Moisés Hidalgo Guerra <mohidalguerra@gmail.com>
2024-03-27 04:16:19 -0500

    Home Bloc API parity
    Updated home bloc to function with snapshot streams

---
+++ app/lib/blocs/home.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:md_notes/resources/notes_provider.dart';
+
+import '../models/note.dart';
+
+class NoteListBloc {
+  NotesProvider _provider = NotesProvider();
+  StreamSubscription<List<Note>>? _subscription;
+  StreamController<List<Note>> _controller = StreamController();
+
+  Stream<List<Note>> get notes => _controller.stream;
+
+  Future<void> fetchMainNotes() async {
+    if (_subscription == null) {
+      _subscription = _provider.mainNotes.listen(
+        (event) {
+          _controller.add(event);
+        },
+      );
+    } else {
+      _controller.add(await _provider.fetchMainNotes());
+    }
+  }
+
+  Future<Note> createNote() async => _provider.createNote();
+
+  dispose() {
+    _subscription?.cancel();
+    _controller.close();
+  }
+}
